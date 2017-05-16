f1b9d2a82de559fda4e0304ddf83acf99f21ad98
Aaron Bond <aaron.bond@outlook.com>
2017-05-16 23:18:33 +0100

    updating tests and readme with travis compatibility

--- src/app/shell/shell.component.spec.ts
+++ src/app/shell/shell.component.spec.ts
@@ -1,13 +1,17 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 
 import { ShellComponent } from './shell.component';
+import { HeaderComponent } from "../header/header.component";
 
 describe('ShellComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [
-				ShellComponent
+				ShellComponent,
+				HeaderComponent
 			],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA]
 		}).compileComponents();
 	}));
 
