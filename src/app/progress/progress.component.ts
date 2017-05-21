e1bcdf9434a4be7c100c7c38fa780cba7f1d5a32
Aaron Bond <aaron.bond@outlook.com>
2017-05-21 13:44:35 +0100

    progress bar now uses actual count of items

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StorageHelper } from "../common/storage-helper";
+import { TodoItemStatus } from "enums";
 
 @Component({
 	selector: 'todo-progress',
@@ -9,6 +10,26 @@ import { StorageHelper } from "../common/storage-helper";
 export class ProgressComponent {
 
 	private todoItems: TodoItem[];
+
+	public get CompletePercentage(): number {
+		if (this.todoItems.length == 0) {
+			return 0;
+		}
+
+		let percentage = (this.todoItems.filter(item => item.Status == TodoItemStatus.Completed).length / this.todoItems.length) * 100;
+
+		return percentage;
+	}
+
+	public get PausedPercentage(): number {
+		if (this.todoItems.length == 0) {
+			return 0;
+		}
+
+		let percentage = (this.todoItems.filter(item => item.Status == TodoItemStatus.Paused).length / this.todoItems.length) * 100;
+
+		return percentage;
+	}
 
 	public constructor(private storageHelper: StorageHelper) {
 		this.todoItems = this.storageHelper.TodoItems;
