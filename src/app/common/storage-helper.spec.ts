832e91a0de222297213cca92e7edb4f1a207b422
aaron-bond <aaron.bond@outlook.com>
2017-05-23 12:21:45 +0100

    fixing formatting

--- src/app/common/storage-helper.spec.ts
+++ src/app/common/storage-helper.spec.ts
@@ -6,16 +6,16 @@ describe('StorageHelper', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({ providers: [StorageHelper] });
     
-    var store = {};
-    spyOn(localStorage, 'getItem').andCallFake(function (key) {
-      return store[key];
-    });
-    spyOn(localStorage, 'setItem').andCallFake(function (key, value) {
-      return store[key] = value + '';
-    });
-    spyOn(localStorage, 'clear').andCallFake(function () {
-        store = {};
-    });
+		var store = {};
+		spyOn(localStorage, 'getItem').andCallFake(function (key) {
+			return store[key];
+		});
+		spyOn(localStorage, 'setItem').andCallFake(function (key, value) {
+			return store[key] = value + '';
+		});
+		spyOn(localStorage, 'clear').andCallFake(function () {
+			store = {};
+		});
 	}));
 
 	it('should do something', async(inject([StorageHelper], (storageHelper: StorageHelper) => {
