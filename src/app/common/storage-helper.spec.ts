c88acd741b712801a96ee7d21d17e0960ae5af1f
aaron-bond <aaron.bond@outlook.com>
2017-05-23 12:19:46 +0100

    Adding a test spec for the storage helper

---
+++ src/app/common/storage-helper.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, async } from '@angular/core/testing';
+
+import { StorageHelper  } from './storage-helper';
+
+describe('StorageHelper', () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({ providers: [StorageHelper] });
+    
+    var store = {};
+    spyOn(localStorage, 'getItem').andCallFake(function (key) {
+      return store[key];
+    });
+    spyOn(localStorage, 'setItem').andCallFake(function (key, value) {
+      return store[key] = value + '';
+    });
+    spyOn(localStorage, 'clear').andCallFake(function () {
+        store = {};
+    });
+	}));
+
+	it('should do something', async(inject([StorageHelper], (storageHelper: StorageHelper) => {
+    
+	})));
+});
